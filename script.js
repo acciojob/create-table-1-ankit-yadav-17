543dbdbea7a67c4aaf479104564ad46346712ce5
ankit-yadav-17 <119732393+ankit-yadav-17@users.noreply.github.com>
2023-06-15 13:48:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+
 function insert_Row() {
-    //Write your code here
-  
-  
+  var table = document.getElementById("sampleTable");
+  var newRow = table.insertRow(0);
+
+  var newCell1 = newRow.insertCell(0);
+  var newCell2 = newRow.insertCell(1);
+
+  newCell1.innerHTML = 'New Cell1';
+  newCell2.innerHTML = 'New Cell2';
 }
